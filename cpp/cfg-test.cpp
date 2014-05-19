e39d34e773c55df0a1e1524c11753300beccc642
Arne Neumann <github@arne.cl>
2014-05-19 12:23:59 +0200

    added ContextfreeGrammar class (C++)

---
+++ cpp/cfg-test.cpp
@@ -0,0 +1,57 @@
+
+#include "cfg.hpp"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    LHS l = "NP";
+    RHS r = {"Det", "N"}; // C++11 initializer list assignment
+    
+    CFGRule rule1 = CFGRule(l, r);
+    std::cout << "rule1: " << rule1 << std::endl;
+
+    CFGRule rule2 = CFGRule("A", {"a", "a"});
+    std::cout << "rule2: " << rule2 << std::endl;
+    
+    if (rule2 < rule1) {std::cout << "rule2 < rule1\n\n";}
+
+    ContextfreeGrammar cfg;
+    cfg.add_rule(CFGRule("S", {"NP", "VP"}));
+    cfg.add_rule(CFGRule("PP", {"P", "NP"}));
+    cfg.add_rule(CFGRule("NP", {"Det", "N"}));
+    cfg.add_rule(CFGRule("NP", {"NP", "PP"}));
+    cfg.add_rule(CFGRule("VP", {"V", "NP"}));
+    cfg.add_rule(CFGRule("VP", {"VP", "PP"}));
+    cfg.add_rule(CFGRule("NP", {"NE"}));
+    cfg.add_rule(CFGRule("NP", {"NE", "NE"}));
+    cfg.add_rule(CFGRule("Det", {"'a'"}));
+    cfg.add_rule(CFGRule("Det", {"'the'"}));
+    cfg.add_rule(CFGRule("Det", {"'one'"}));
+    cfg.add_rule(CFGRule("N", {"'dog'"}));
+    cfg.add_rule(CFGRule("N", {"'cat'"}));
+    cfg.add_rule(CFGRule("N", {"'politician'"}));
+    cfg.add_rule(CFGRule("V", {"'chased'"}));
+    cfg.add_rule(CFGRule("V", {"'sat'"}));
+    cfg.add_rule(CFGRule("V", {"'killed'"}));
+    cfg.add_rule(CFGRule("P", {"'on'"}));
+    cfg.add_rule(CFGRule("P", {"'in'"}));
+    cfg.add_rule(CFGRule("NE", {"'David'"}));
+    cfg.add_rule(CFGRule("NE", {"'Hasselhoff'"}));
+    cfg.add_rule(CFGRule("NE", {"'George'"}));
+    cfg.add_rule(CFGRule("NE", {"'Micheal'"}));
+    
+    std::cout << "toy grammar with " << cfg.size() << " rules:" << std::endl;
+    std::cout << cfg << std::endl;
+    
+    std::cout << "rules w/ 'NP' as lhs:" << std::endl;
+    std::set<CFGRule> nt_rules = cfg.rules_for("NP");
+    for (std::set<CFGRule>::const_iterator r = nt_rules.begin();
+        r!= nt_rules.end(); ++r) {
+            std::cout << "\t" << *r;
+        }
+        
+    std::cout << "\n\nstart symbol before setting it: " << cfg.get_startsym() << std::endl;
+    cfg.set_startsym("S");
+    std::cout << "start symbol after setting it: " << cfg.get_startsym() << std::endl;
+}
---
+++ cpp/cfg.hpp
@@ -0,0 +1,102 @@
+// a context free grammar class (C++11)
+// compiles with: g++/clang++ -std=c++11 cfg-test.cpp
+// Arne Neumann, 2014-05-19
+
+#include <vector>
+#include <set>
+#include <iostream>
+#include <string>
+
+typedef std::string LHS;
+typedef std::vector<std::string> RHS;
+typedef std::string Nonterminal;
+typedef std::string Terminal;
+typedef std::string StartSymbol;
+
+class CFGRule
+{
+private: // instance variables
+    LHS lhs;
+    RHS rhs;
+
+public:
+    CFGRule(const LHS& l, const RHS& r)
+    {
+        lhs = l;
+        rhs = r;
+    }
+
+    const LHS& get_lhs() const {return lhs;}
+    
+    bool operator<(const CFGRule& other) const
+    {
+        if (get_lhs() < other.get_lhs()) {return true;}
+        if (rhs < other.rhs) {return true;}
+        return false;
+    }
+    
+  /// prints CFGRule in 'A -> B C D' style
+  void print(std::ostream& o) const
+  {
+    o << get_lhs() << " -> ";
+    for (RHS::const_iterator e = rhs.begin(); e != rhs.end(); ++e) {
+      o << *e << " ";
+    }
+    o << std::endl;
+  }
+}; // CFGRule
+
+
+// can't define this before CFGRule is defined
+typedef std::set<CFGRule> Rules;
+
+class ContextfreeGrammar
+{
+private: // instance variables
+    std::set<Nonterminal> nts; // so far unused
+    std::set<Terminal> ts; // so far unused
+    StartSymbol startsym;
+    Rules rules;
+
+public:
+    const unsigned size() const {return rules.size();}
+
+    const StartSymbol& get_startsym() const {return startsym;}
+    
+    void set_startsym(const StartSymbol& s) {startsym = s;}
+    
+    void add_rule(const CFGRule& r) {rules.insert(r);}
+    
+    /// returns all rules w/ nt as lhs
+    const Rules rules_for(const Nonterminal& nt) const {
+        Rules nt_rules;
+        for (Rules::const_iterator it = rules.begin();
+            it != rules.end(); ++it) {
+                if (it->get_lhs() == nt) {nt_rules.insert(*it);}
+        }
+        return nt_rules; // can't return ref to local object
+    }
+    
+    /// prints all CFG rules, one per line
+    void print(std::ostream& o) const
+    {
+        for (Rules::const_iterator r = rules.begin();
+            r != rules.end(); ++r) {
+            r->print(o);
+        }
+    }
+}; // ContextfreeGrammar
+
+/// overloaded output operator for CFGRule
+std::ostream& operator<<(std::ostream& o, const CFGRule& r)
+{
+  r.print(o);
+  return o;
+}
+
+/// overloaded output operator for ContextfreeGrammar
+std::ostream& operator<<(std::ostream& o, const ContextfreeGrammar& cfg)
+{
+  cfg.print(o);
+  return o;
+}
