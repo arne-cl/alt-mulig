580bc96e97c837a8083a7f116de2a412a927c95b
Arne Neumann <github@arne.cl>
2014-05-24 23:58:58 +0200

    cfg: added nonterminal print to test

--- cpp/cfg-test.cpp
+++ cpp/cfg-test.cpp
@@ -7,13 +7,13 @@ int main()
 {
     CFGRule::Symbol l = "NP";
     CFGRule::SymbolVector r = {"Det", "N"}; // C++11 initializer list assignment
-    
+
     CFGRule rule1 = CFGRule(l, r);
     std::cout << "rule1: " << rule1 << std::endl;
 
     CFGRule rule2 = CFGRule("A", {"a", "a"});
     std::cout << "rule2: " << rule2 << std::endl;
-    
+
     if (rule2 < rule1) {std::cout << "rule2 < rule1\n\n";}
 
     ContextfreeGrammar cfg;
@@ -40,17 +40,24 @@ int main()
     cfg.add_rule(CFGRule("NE", {"'Hasselhoff'"}));
     cfg.add_rule(CFGRule("NE", {"'George'"}));
     cfg.add_rule(CFGRule("NE", {"'Micheal'"}));
-    
+
     std::cout << "toy grammar with " << cfg.size() << " rules:" << std::endl;
     std::cout << cfg << std::endl;
-    
+
+    std::cout << "nonterminals: " << std::endl;
+    const ContextfreeGrammar::SymbolSet& nts = cfg.get_nonterminals();
+    for (ContextfreeGrammar::SymbolSetIter s = nts.begin(); s != nts.end(); ++s) {
+        std::cout << "\t" << *s << " ";
+    }
+    std::cout << std::endl;
+
     std::cout << "rules w/ 'NP' as lhs:" << std::endl;
     std::set<CFGRule> nt_rules = cfg.rules_for("NP");
     for (std::set<CFGRule>::const_iterator r = nt_rules.begin();
         r!= nt_rules.end(); ++r) {
             std::cout << "\t" << *r;
         }
-        
+
     std::cout << "\n\nstart symbol before setting it: " << cfg.get_startsymbol() << std::endl;
     cfg.set_startsymbol("S");
     std::cout << "start symbol after setting it: " << cfg.get_startsymbol() << std::endl;
