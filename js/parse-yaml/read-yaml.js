229accaf69139196a2b2b319a1632f70bd35ced1
Arne Neumann <github@arne.cl>
2018-10-21 22:51:37 +0200

    js yaml: get rst parser names

--- js/parse-yaml/read-yaml.js
+++ js/parse-yaml/read-yaml.js
@@ -1,22 +1,37 @@
-"use strict";
-
 // NOTE: loading a local file will only work if we serve this directory
 // from a webserver, even if it's just "python -m SimpleHTTPServer".
 $(document).ready(function () {
   parseYAML('docker-compose.yml');
 });
 
-
-function parseYAML(yamlFilepath) {
-    $.get(yamlFilepath)
+function parseYAML(filepath) {
+    $.get(filepath)
     .done(function (data) {
         var yamlObject = jsyaml.safeLoad(data);
-        $("#results").append(yamlObject);
+        var jsonString = JSON.stringify(data);
+        var jsonObject = $.parseJSON(jsonString);
+        addToResults(jsonObject);
+
+        var rstParsers = getRSTParsers(yamlObject);
+        addToResults(rstParsers);
     });
 }
 
+// getRSTParsers returns the names of all RST parsers from the object
+// representation of a docker-compose.yml file.
+function getRSTParsers(yamlObject) {
+    var parsers = [];
+    for (var service of Object.keys(yamlObject.services)) {
+        serviceType = yamlObject.services[service].build.labels.type
+        if (serviceType === 'rst-parser' ) {
+            parsers.push(service);
+        }
+    }
+    return parsers;
+}
+
 function addToResults(thing) {
-    $("#results").append(thing + '<br>\n');
+    $("#results").append(thing + '<br><br>\n');
 }
 
 //~ Object.keys(yamlObject['services']).forEach(addToResults)
