84eebd0e4fa10a22e9c313dc6391c20f84f2adf5
Arne Neumann <github@arne.cl>
2023-05-14 13:58:17 +0200

    characteristic equation: more tests

--- python/recurrence_relations/test_recurrence.py
+++ python/recurrence_relations/test_recurrence.py
@@ -66,6 +66,43 @@ def test_char_eqn_roots_6():
     assert all(abs(eqn.subs(sp.symbols('r'), root).evalf()) < 1e-10 for root in roots)
 
 
+from sympy import symbols, Eq, solve, simplify
+
+r = symbols('r')
+
+def test_char_eqn_roots_symbolic_1():
+    # Test case 1: r**3 - 3*r**2 + 2*r - 1
+    eqn, roots = char_eqn_roots([1, -3, 2, -1])
+    for root in roots:
+        assert simplify(eqn.subs(r, root)) == 0
+    expected_roots = solve(Eq(r**3 - 3*r**2 + 2*r - 1, 0), r)
+    assert set(roots) == set(expected_roots)
+
+def test_char_eqn_roots_symbolic_2():
+    # Test case 2: r**2 - r + 1
+    eqn, roots = char_eqn_roots([1, -1, 1])
+    for root in roots:
+        assert simplify(eqn.subs(r, root)) == 0
+    expected_roots = solve(Eq(r**2 - r + 1, 0), r)
+    assert set(roots) == set(expected_roots)
+
+def test_char_eqn_roots_symbolic_3():
+    # Test case 3: r**3 - 2*r**2 - r + 1
+    eqn, roots = char_eqn_roots([1, -2, -1, 1])
+    for root in roots:
+        assert simplify(eqn.subs(r, root)) == 0
+    expected_roots = solve(Eq(r**3 - 2*r**2 - r + 1, 0), r)
+    assert set(roots) == set(expected_roots)
+
+def test_char_eqn_roots_symbolic_4():
+    # Test case 4: r**3 + r**2 - 2*r + 3
+    eqn, roots = char_eqn_roots([1, 1, -2, 3])
+    for root in roots:
+        assert simplify(eqn.subs(r, root)) == 0
+    expected_roots = solve(Eq(r**3 + r**2 - 2*r + 3, 0), r)
+    assert set(roots) == set(expected_roots)
+
+
 # Run the tests
 pytest.main()
 
