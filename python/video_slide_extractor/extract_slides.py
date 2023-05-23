738a3597bdfabc7a408e8d54b8c06f73b7fed465
Arne Neumann <git@arne.cl>
2023-05-23 15:44:48 +0200

    video_slide_extractor: first OO working prototype

--- python/video_slide_extractor/extract_slides.py
+++ python/video_slide_extractor/extract_slides.py
@@ -7,13 +7,12 @@ import os
 from skimage.metrics import structural_similarity as ssim
 import time
 
+
 def open_video(video_path):
     cap = cv2.VideoCapture(video_path)
 
-    if not cap.isOpened():
-        print(f"Error opening video file {video_path}")
-        return None
-
+    if cap is None or not cap.isOpened():
+        raise Exception(f"Error opening video file {video_path}")
     return cap
 
 def get_video_info(cap):
@@ -23,60 +22,61 @@ def get_video_info(cap):
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     print(f"Video information: Frame rate: {frame_rate}, Frame width: {frame_width}, Frame height: {frame_height}, Total frames: {total_frames}")
-
     return frame_rate, frame_width, frame_height, total_frames
 
 def compare_frames(frame1, frame2):
     grayA = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    score = ssim(grayA, grayB)
+    return ssim(grayA, grayB)
 
-    return score
 
-def extract_slides(video_path, output_prefix, ssim_threshold, consecutive_frames):
-    cap = open_video(video_path)
-    if cap is None:
-        return
+class SlideExtractor(object):
+    def __init__(self, video_path, output_prefix, ssim_threshold, consecutive_frames):
+        self.video_path = video_path
+        self.output_prefix = output_prefix
+        self.ssim_threshold = ssim_threshold
+        self.consecutive_frames = consecutive_frames
 
-    frame_rate, frame_width, frame_height, total_frames = get_video_info(cap)
+    def extract_slides(self):
+        cap = open_video(self.video_path)
+        frame_rate, frame_width, frame_height, total_frames = get_video_info(cap)
 
-    ret, comparison_frame = cap.read()
-    if not ret:
-        print("Error reading the first frame")
-        return
-
-    frame_count = 1
-    similar_frames = 0
-    slide_count = 0
-    start_time = time.time()
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
+        ret, comparison_frame = cap.read()
         if not ret:
-            break
+            raise Exception("Error reading the first frame")
 
-        frame_count += 1
+        self.frame_count = 1
+        self.similar_frames = 0
+        self.slide_count = 0
+        self.start_time = time.time()
 
-        if compare_frames(comparison_frame, frame) >= ssim_threshold:
-            similar_frames += 1
-            if similar_frames == consecutive_frames:
-                timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-                cv2.imwrite(f"{output_prefix}-{timestamp}.png", comparison_frame)
-                slide_count += 1
-                print(f"Extracted slide {slide_count} at timestamp {timestamp}s")
-        else:
-            comparison_frame = frame
-            similar_frames = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        # Progress indicator
-        if time.time() - start_time >= 1:  # Update progress every second of real time
-            progress = (frame_count / total_frames) * 100
-            print(f"Progress: {progress:.2f}%, Frame: {frame_count}")
-            start_time = time.time()
+            self.frame_count += 1
 
-    cap.release()
-    cv2.destroyAllWindows()
+            if compare_frames(comparison_frame, frame) >= self.ssim_threshold:
+                self.similar_frames += 1
+            else: # frame is not similar to comparison_frame
+                if self.similar_frames >= self.consecutive_frames:
+                    timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
+                    cv2.imwrite(f"{self.output_prefix}-{timestamp}.png", comparison_frame)
+                    self.slide_count += 1
+                    print(f"Extracted slide {self.slide_count} at timestamp {timestamp}s")
+
+                comparison_frame = frame
+                self.similar_frames = 0
+
+            # Progress indicator
+            if time.time() - self.start_time >= 1:  # Update progress every second of real time
+                progress = (self.frame_count / total_frames) * 100
+                print(f"Progress: {progress:.2f}%, Frame: {self.frame_count}")
+                start_time = time.time()
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
@@ -87,9 +87,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    video_path = args.video_path
-    ssim_threshold = args.ssim_threshold
-    consecutive_frames = args.consecutive_frames
-    output_prefix = os.path.splitext(video_path)[0]  # Remove file extension
-
-    extract_slides(video_path, output_prefix, ssim_threshold, consecutive_frames)
+    output_prefix = os.path.splitext(args.video_path)[0]  # Remove file extension
+    extractor = SlideExtractor(args.video_path, output_prefix, args.ssim_threshold, args.consecutive_frames)
+    extractor.extract_slides()
